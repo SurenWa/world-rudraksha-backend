29f7a3e718e2840375f239491d04a3a8d2f2900e
Surendra Wagle <cirrusminor2@gmail.com>
2025-03-31 17:09:15 +0545

    region-test-4

--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -20,13 +20,10 @@ export class S3Service {
     private readonly awsRegion: string;
 
     constructor(private configService: ConfigService) {
-        this.bucketName =
-            this.configService.get<string>('AWS_S3_BUCKET_NAME') ||
-            'world-rudraksha-bucket';
-        this.awsRegion =
-            this.configService.get<string>('AWS_REGION') || 'us-east-2';
+        this.bucketName = this.configService.get<string>('AWS_S3_BUCKET_NAME');
+        this.awsRegion = this.configService.get<string>('AWS_REGION');
         this.s3Client = new S3Client({
-            region: this.configService.get<string>('AWS_REGION') || 'us-east-2',
+            region: this.configService.get<string>('AWS_REGION'),
             credentials: {
                 accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY'),
                 secretAccessKey: this.configService.get<string>(
